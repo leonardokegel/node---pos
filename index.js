232e13689824b2cbf84126114614bbc6d7f8ba44
Leonardo Kegel <leonardo.kegel@gmail.com>
2022-06-27 22:06:52 -0300

    altera index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.get("/", (request, response) => {
+    return response.send("olá mundo!");
+});
 app.listen(port, () => {
-  console.info("App rodando na porta 3333");
+    console.info("App rodando na porta 3333");
 });
