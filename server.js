18ab2da9ce5feb08e664afd74ed9bbdf5d292d59
Leonardo Kegel <leonardo.kegel@gmail.com>
2022-07-10 19:05:56 -0300

    adiciona rota get produtos por id

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const port = process.env.PORT || 3333;
 
 const lista_produtos = {
     produtos: [
-        { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  },
-        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  },
-        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé"  },
-        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps"  },
-        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé"  },
+        { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João" },
+        { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans" },
+        { id: 3, descricao: "Iogurte Natural 200ml", valor: 2.50, marca: "Itambé" },
+        { id: 4, descricao: "Batata Maior Palha 300gr", valor: 15.20, marca: "Chipps" },
+        { id: 5, descricao: "Nescau 400gr", valor: 8.00, marca: "Nestlé" },
     ]
 };
 
@@ -23,6 +23,25 @@ app.listen(port, () => {
     console.info("App rodando na porta 3333");
 });
 
+
+app.get("/", (req, res) => {
+    return res.send('Leonardo Kegel Porto Testa');
+});
+
 app.get("/produtos", (req, res) => {
     return res.json(lista_produtos);
+});
+
+app.get("/produtos/:id", (req, res) => {
+    let id = +req.params.id
+    let idx = lista_produtos.find((e) => e.id === id);
+
+    if (idx) {
+        res.json(idx);
+    } else {
+        res.status(404).json({
+            message: "produto não encontrado"
+        });
+    }
+
 });
